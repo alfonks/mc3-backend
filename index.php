5a00e1d99423ac7f91d7d88b94fce73cae730fd8
Alfon <alfonsusanto@gmail.com>
2020-07-24 00:37:48 +0700

    tambah halaman index

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <title>Data Travel MC3</title>
+  </head>
+  <body>
+    <form action="input_data.php">
+      <button type="submit" class="btn btn-primary">Ke input data</button>
+    </form>
+    <br>
+
+    <form action="list_restoran.php">
+      <button type="submit" class="btn btn-primary">Lihat semua data restoran & Kasih rating</button>
+    </form>
+    <br>
+
+  </body>
+</html>
